a51555f065863ea48b980d0cb6ff40c0011ee581
rasveerb <rasveerb@sky.com>
2020-06-10 16:51:12 +0100

    fixed the POST method to talk to the form

--- server.js
+++ server.js
@@ -1,28 +1,24 @@
 var express = require('express');
 var app = express();
 
-//POST method
-app.get("/create-post", function(req,res){
-  res.send("blogpost page");
-})
-
-app.post("/create-post", function(req,res){
-  console.log("/create-post");
-})
-
-app.get("/", function(req, res) {
-  res.send("Yay it works!");
-});
-
-app.get("/about", function(req, res){
-  res.send("My name is Ras");
-});
-
 //routes specific image to a specific path
 app.use("/frank",express.static('public/quitefrankly logo.png'));
 
 //adds in all static content in public folder
 app.use(express.static('public'));
+
+//serves the html page when you hit localhost
+app.get("/", function(req,res){
+  res.sendFile(process.cwd() + "/public/index.html");
+});
+//POST method
+app.post("/create-post", function(req,res){
+  console.log("/create-post");
+})
+
+app.get("/about", function(req, res){
+  res.send("My name is Ras");
+});
 
 app.listen(3000, function(){
   console.log('Server is now listening on port 3000. Ready to accept requests!');

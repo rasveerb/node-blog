b6b5f58b58f02a62bde190d2f5d1571644f3f879
rasveerb <rasveerb@sky.com>
2020-06-11 13:35:56 +0100

    WIP got writefile write blogpost entries to posts.json

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var formidable = require('express-formidable');
+var fs = require('fs');
 
 var app = express();
 
@@ -18,7 +19,18 @@ app.get("/", function(req,res){
 //POST method
 app.post("/create-post", function(req,res){
   console.log(req.fields);
-})
+});
+
+// //method which writes data to the hard drive - NEED TO REFACTOR
+fs.writeFile(__dirname + '/data/posts.json',JSON.stringify(blogpost = {"2020210": "test content"}, null, 2), function(error){ //__dirname = global node object - gives path to current working directory
+  if(error) throw (error);
+});
+
+//method which reads data that's already there
+fs.readFile(__dirname + '/data/posts.json', function(error,file){
+  var parsedFile = JSON.parse(file);
+  console.log(parsedFile);
+});
 
 app.get("/about", function(req, res){
   res.send("My name is Ras");

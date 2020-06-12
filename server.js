724ac31cf045e8d9993160c8eca9d6f4c39333b8
rasveerb <rasveerb@sky.com>
2020-06-12 13:30:51 +0100

    added timestamp to posts.json objects

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-//TODO make a thank you submissions page when clicked submit
 //TODO get all recent posts to show as soon as you click submit
 var express = require('express');
 var formidable = require('express-formidable');
@@ -14,38 +13,40 @@ const timestamp = Date.now();
 //adds in all static content in public folder
 app.use(express.static('public'));
 //routes specific image to a specific path
-app.use("/frank",express.static('public/quitefrankly logo.png'));
+app.use("/frank", express.static('public/quitefrankly logo.png'));
 
 app.use(formidable());
 
 //serves the html page when you hit localhost
-app.get("/", function(req,res){
+app.get("/", function(req, res) {
   res.sendFile(process.cwd() + "/public/index.html");
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function(req, res) {
   res.send("My name is Ras");
 });
 
 //POST method
-app.post("/create-post", function(req,res){
+app.post("/create-post", function(req, res) {
 
   //TODO change the blogpost to include the Date.now() as the key and post as the value
-console.log("timestamp: ", timestamp);
+  console.log("timestamp: ", timestamp);
 
-  dataObject = { timestamp : req.fields.blogpost};
+  dataObject = {
+    [timestamp]: req.fields.blogpost
+  };
 
-  console.log ("data object: ", dataObject);
+  console.log("data object: ", dataObject);
 
   postsArray.push(dataObject);
 
-  fs.writeFile(__dirname + '/data/posts.json', JSON.stringify(postsArray), function(error){ //maybe try append file and read out an array somehow
-    if(error) throw(error);
+  fs.writeFile(__dirname + '/data/posts.json', JSON.stringify(postsArray), function(error) { //maybe try append file and read out an array somehow
+    if (error) throw (error);
   });
 
-  fs.readFile(__dirname + '/data/posts.json', function(error,file){
+  fs.readFile(__dirname + '/data/posts.json', function(error, file) {
     var parsedFile = JSON.parse(file);
-    console.log("read file inside POST: " ,parsedFile);
+    console.log("read file inside POST: ", parsedFile);
   });
 
 });
@@ -56,11 +57,11 @@ console.log("timestamp: ", timestamp);
 // });
 //
 // //method which reads data that's already there
-fs.readFile(__dirname + '/data/posts.json', function(error,file){
+fs.readFile(__dirname + '/data/posts.json', function(error, file) {
   var parsedFile = JSON.parse(file);
-  console.log("read file: " ,parsedFile);
+  console.log("read file: ", parsedFile);
 });
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log('Server is now listening on port 3000. Ready to accept requests!');
 });
